acd3f0aa17343407ed3274084a0aabe6631d6006
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2024-01-08 13:22:26 +0100

    Profile Web - all in .

--- src/data/TecsData.js
+++ src/data/TecsData.js
@@ -1,67 +1,67 @@
 export const Tecs = [
   {
-    imagenSrc: "src/assets/iconos/javascript.svg",
+    imagenSrc: "./src/assets/iconos/javascript.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/html.svg",
+    imagenSrc: "./src/assets/iconos/html.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/css.svg",
+    imagenSrc: "./src/assets/iconos/css.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/npm.svg",
+    imagenSrc: "./src/assets/iconos/npm.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/nodejs.svg",
+    imagenSrc: "./src/assets/iconos/nodejs.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/reactjs.svg",
+    imagenSrc: "./src/assets/iconos/reactjs.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/vitejs.svg",
+    imagenSrc: "./src/assets/iconos/vitejs.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/sass.svg",
+    imagenSrc: "./src/assets/iconos/sass.svg",
   },
 // 
   {
-    imagenSrc: "src/assets/iconos/php.svg",
+    imagenSrc: "./src/assets/iconos/php.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/mysql.svg",
+    imagenSrc: "./src/assets/iconos/mysql.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/python.svg",
+    imagenSrc: "./src/assets/iconos/python.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/java.svg",
+    imagenSrc: "./src/assets/iconos/java.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/vue.svg",
+    imagenSrc: "./src/assets/iconos/vue.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/angular.svg",
+    imagenSrc: "./src/assets/iconos/angular.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/mongodb.svg",
+    imagenSrc: "./src/assets/iconos/mongodb.svg",
   },
   // 
   {
-    imagenSrc: "src/assets/iconos/github.svg",
+    imagenSrc: "./src/assets/iconos/github.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/visualcode.svg",
+    imagenSrc: "./src/assets/iconos/visualcode.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/trello.svg",
+    imagenSrc: "./src/assets/iconos/trello.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/figma.svg",
+    imagenSrc: "./src/assets/iconos/figma.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/photoshop.svg",
+    imagenSrc: "./src/assets/iconos/photoshop.svg",
   },
   {
-    imagenSrc: "src/assets/iconos/typescript.svg",
+    imagenSrc: "./src/assets/iconos/typescript.svg",
   },
 ];
