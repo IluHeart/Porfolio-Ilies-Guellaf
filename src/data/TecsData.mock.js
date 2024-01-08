dc9aee903f76d3565f45e3ab0b402102622ca44a
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2024-01-08 15:36:09 +0100

    Profile Web - add vercel . json

--- src/data/TecsData.mock.js
+++ src/data/TecsData.mock.js
@@ -1,67 +1,67 @@
 export const Tecs = [
   {
-    imagenSrc: "/src/assets/javascript.svg",
+    imagenSrc: "./src/assets/javascript.svg",
   },
   {
-    imagenSrc: "/src/assets/html.svg",
+    imagenSrc: "./src/assets/html.svg",
   },
   {
-    imagenSrc: "/src/assets/css.svg",
+    imagenSrc: "./src/assets/css.svg",
   },
   {
-    imagenSrc: "/src/assets/npm.svg",
+    imagenSrc: "./src/assets/npm.svg",
   },
   {
-    imagenSrc: "/src/assets/nodejs.svg",
+    imagenSrc: "./src/assets/nodejs.svg",
   },
   {
-    imagenSrc: "/src/assets/reactjs.svg",
+    imagenSrc: "./src/assets/reactjs.svg",
   },
   {
-    imagenSrc: "/src/assets/vitejs.svg",
+    imagenSrc: "./src/assets/vitejs.svg",
   },
   {
-    imagenSrc: "/src/assets/sass.svg",
+    imagenSrc: "./src/assets/sass.svg",
   },
 // 
   {
-    imagenSrc: "/src/assets/php.svg",
+    imagenSrc: "./src/assets/php.svg",
   },
   {
-    imagenSrc: "/src/assets/mysql.svg",
+    imagenSrc: "./src/assets/mysql.svg",
   },
   {
-    imagenSrc: "/src/assets/python.svg",
+    imagenSrc: "./src/assets/python.svg",
   },
   {
-    imagenSrc: "/src/assets/java.svg",
+    imagenSrc: "./src/assets/java.svg",
   },
   {
-    imagenSrc: "/src/assets/vue.svg",
+    imagenSrc: "./src/assets/vue.svg",
   },
   {
-    imagenSrc: "/src/assets/angular.svg",
+    imagenSrc: "./src/assets/angular.svg",
   },
   {
-    imagenSrc: "/src/assets/mongodb.svg",
+    imagenSrc: "./src/assets/mongodb.svg",
   },
   // 
   {
-    imagenSrc: "/src/assets/github.svg",
+    imagenSrc: "./src/assets/github.svg",
   },
   {
-    imagenSrc: "/src/assets/visualcode.svg",
+    imagenSrc: "./src/assets/visualcode.svg",
   },
   {
-    imagenSrc: "/src/assets/trello.svg",
+    imagenSrc: "./src/assets/trello.svg",
   },
   {
-    imagenSrc: "/src/assets/figma.svg",
+    imagenSrc: "./src/assets/figma.svg",
   },
   {
-    imagenSrc: "/src/assets/photoshop.svg",
+    imagenSrc: "./src/assets/photoshop.svg",
   },
   {
-    imagenSrc: "/src/assets/typescript.svg",
+    imagenSrc: "./src/assets/typescript.svg",
   },
 ];
