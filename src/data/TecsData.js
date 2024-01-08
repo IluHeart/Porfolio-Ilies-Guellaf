ebdd2388a9e37d4dfab2c7f5f8fbb8609e66c029
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2024-01-08 14:37:21 +0100

    Profile Web - Trying Vercel work

--- src/data/TecsData.js
+++ src/data/TecsData.js
@@ -1,67 +1,67 @@
 export const Tecs = [
   {
-    imagenSrc: "/src/assets/iconos/javascript.svg",
+    imagenSrc: "/src/assets/javascript.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/html.svg",
+    imagenSrc: "/src/assets/html.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/css.svg",
+    imagenSrc: "/src/assets/css.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/npm.svg",
+    imagenSrc: "/src/assets/npm.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/nodejs.svg",
+    imagenSrc: "/src/assets/nodejs.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/reactjs.svg",
+    imagenSrc: "/src/assets/reactjs.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/vitejs.svg",
+    imagenSrc: "/src/assets/vitejs.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/sass.svg",
+    imagenSrc: "/src/assets/sass.svg",
   },
 // 
   {
-    imagenSrc: "/src/assets/iconos/php.svg",
+    imagenSrc: "/src/assets/php.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/mysql.svg",
+    imagenSrc: "/src/assets/mysql.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/python.svg",
+    imagenSrc: "/src/assets/python.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/java.svg",
+    imagenSrc: "/src/assets/java.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/vue.svg",
+    imagenSrc: "/src/assets/vue.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/angular.svg",
+    imagenSrc: "/src/assets/angular.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/mongodb.svg",
+    imagenSrc: "/src/assets/mongodb.svg",
   },
   // 
   {
-    imagenSrc: "/src/assets/iconos/github.svg",
+    imagenSrc: "/src/assets/github.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/visualcode.svg",
+    imagenSrc: "/src/assets/visualcode.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/trello.svg",
+    imagenSrc: "/src/assets/trello.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/figma.svg",
+    imagenSrc: "/src/assets/figma.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/photoshop.svg",
+    imagenSrc: "/src/assets/photoshop.svg",
   },
   {
-    imagenSrc: "/src/assets/iconos/typescript.svg",
+    imagenSrc: "/src/assets/typescript.svg",
   },
 ];
