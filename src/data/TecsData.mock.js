636da91816c35ad6b38537c0264f3c85274832e8
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2024-01-08 16:46:39 +0100

    Profile Web - add

--- src/data/TecsData.mock.js
+++ src/data/TecsData.mock.js
@@ -1,67 +1,67 @@
 export const Tecs = [
   {
-    imagenSrc: "./public/iconos/javascript.svg",
+    imagenSrc: "/iconos/javascript.svg",
   },
   {
-    imagenSrc: "./public/iconos/html.svg",
+    imagenSrc: "/iconos/html.svg",
   },
   {
-    imagenSrc: "./public/iconos/css.svg",
+    imagenSrc: "/iconos/css.svg",
   },
   {
-    imagenSrc: "./public/iconos/npm.svg",
+    imagenSrc: "/iconos/npm.svg",
   },
   {
-    imagenSrc: "./public/iconos/nodejs.svg",
+    imagenSrc: "/iconos/nodejs.svg",
   },
   {
-    imagenSrc: "./public/iconos/reactjs.svg",
+    imagenSrc: "/iconos/reactjs.svg",
   },
   {
-    imagenSrc: "./public/iconos/vitejs.svg",
+    imagenSrc: "/iconos/vitejs.svg",
   },
   {
-    imagenSrc: "./public/iconos/sass.svg",
+    imagenSrc: "/iconos/sass.svg",
   },
 // 
   {
-    imagenSrc: "./public/iconos/php.svg",
+    imagenSrc: "/iconos/php.svg",
   },
   {
-    imagenSrc: "./public/iconos/mysql.svg",
+    imagenSrc: "/iconos/mysql.svg",
   },
   {
-    imagenSrc: "./public/iconos/python.svg",
+    imagenSrc: "/iconos/python.svg",
   },
   {
-    imagenSrc: "./public/iconos/java.svg",
+    imagenSrc: "/iconos/java.svg",
   },
   {
-    imagenSrc: "./public/iconos/vue.svg",
+    imagenSrc: "/iconos/vue.svg",
   },
   {
-    imagenSrc: "./public/iconos/angular.svg",
+    imagenSrc: "/iconos/angular.svg",
   },
   {
-    imagenSrc: "/public/iconos/mongodb.svg",
+    imagenSrc: "/iconos/mongodb.svg",
   },
   // 
   {
-    imagenSrc: "./public/iconos/github.svg",
+    imagenSrc: "/iconos/github.svg",
   },
   {
-    imagenSrc: "./public/iconos/visualcode.svg",
+    imagenSrc: "/iconos/visualcode.svg",
   },
   {
-    imagenSrc: "./public/iconos/trello.svg",
+    imagenSrc: "/iconos/trello.svg",
   },
   {
-    imagenSrc: "./public/iconos/figma.svg",
+    imagenSrc: "/iconos/figma.svg",
   },
   {
-    imagenSrc: "./public/iconos/photoshop.svg",
+    imagenSrc: "/Imagen de respaldo/IMG_0815.JPG",
   },
   {
-    imagenSrc: "./public/iconos/typescript.svg",
+    imagenSrc: ("/src/assets/background/background.svg"),
   },
 ];
