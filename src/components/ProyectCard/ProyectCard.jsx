e7cc39ed2c0d862d9129ddf888669d824b2af8cc
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2024-01-17 20:15:19 +0100

    Add first card

--- src/components/ProyectCard/ProyectCard.jsx
+++ src/components/ProyectCard/ProyectCard.jsx
@@ -1,13 +1,30 @@
 /* eslint-disable react/prop-types */
-import styles from "./ProyectCard.module.scss"
+import styles from "./ProyectCard.module.scss";
 
-const ProyectCard = ({ title, description }) => {
-    return (
-      <div className={styles.proyectCard}>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.description}>{description}</div>
+const ProyectCard = ({
+  imagenSrc,
+  title,
+  description,
+  githubLink,
+  webLink,
+}) => {
+  return (
+    <div className={styles.proyectCard}>
+      <img className={styles.imgCard} src={imagenSrc} alt="" />
+      <div className={styles.info}>
+        <h4>{title}</h4>
+        <p>{description}</p>
+        <div className={styles.buttonContainer}>
+          <button className={styles.gitButton} onClick={() => window.open(githubLink, "_blank")}>
+            GITHUB
+          </button>
+          <button className={styles.webButton} onClick={() => window.open(webLink, "_blank")}>
+            PAGINA WEB
+          </button>
+        </div>
       </div>
-    );
-  };
-  
-  export default ProyectCard;
+    </div>
+  );
+};
+
+export default ProyectCard;
--- src/pages/Proyectos/Proyectos.jsx
+++ src/pages/Proyectos/Proyectos.jsx
@@ -1,15 +1,19 @@
 import styles from "./Proyectos.module.scss";
 import Section from "../../components/ContainerSelectors/Section/Section";
+import ProyectCard from "../../components/ProyectCard/ProyectCard";
 
-const Pyectos = () => {
-    return (
-      <div className={styles.Proyectos}>
-        <Section>
-            
-        </Section>
-      </div>
-    );
-  };
-  
-  export default Pyectos;
-  
+const Proyectos = () => {
+  return (
+    <div className={styles.Proyectos}>
+      <Section>
+        <ProyectCard
+          imagenSrc="./imagenes de los proyectos/Miniatura EA.jpg"
+          title="Replicación de la Página Web de Electronic Arts en React"
+          description={"Este fue el proyecto que presentamos al final del bootcamp para mostrar el uso de componentes reutilizables y manejables con Reactjs"}
+        />
+      </Section>
+    </div>
+  );
+};
+
+export default Proyectos;
